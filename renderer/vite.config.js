c2f9e936237bd6c0aeb3f8443983f146f4de072f
Andrew Hou <andrewhou12@gmail.com>
2025-07-06 01:41:20 -0700

    newest

--- renderer/vite.config.js
+++ renderer/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite"
 
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import tailwind from 'tailwindcss'
+
 
 
 // https://vite.dev/config/
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       input: {
