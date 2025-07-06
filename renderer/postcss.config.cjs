7826ad52e3c888adc9abc52e877a6b3fd263ca7c
Andrew Hou <andrewhou12@gmail.com>
2025-07-06 01:07:37 -0700

    newest

---
+++ renderer/postcss.config.cjs
@@ -0,0 +1,11 @@
+const postcssImport = require('postcss-import');
+const tailwindPostcss = require('@tailwindcss/postcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    postcssImport(),
+    tailwindPostcss(),
+    autoprefixer(),
+  ],
+};
