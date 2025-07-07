5db1461b978a4a42f31eff69e08faaed3c1c483e
Andrew Hou <andrewhou12@gmail.com>
2025-07-06 23:54:35 -0700

    fixed tailwind bugs

--- renderer/postcss.config.cjs
+++ renderer/postcss.config.cjs
@@ -1,11 +1,17 @@
-const postcssImport = require('postcss-import');
-const tailwindPostcss = require('@tailwindcss/postcss');
-const autoprefixer = require('autoprefixer');
+// const postcssImport = require('postcss-import');
+// const tailwindPostcss = require('@tailwindcss/postcss');
+// const autoprefixer = require('autoprefixer');
+
+// module.exports = {
+//   plugins: [
+//     postcssImport(),
+//     tailwindPostcss(),
+//     autoprefixer(),
+//   ],
+// };
 
 module.exports = {
-  plugins: [
-    postcssImport(),
-    tailwindPostcss(),
-    autoprefixer(),
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+  },
+}
