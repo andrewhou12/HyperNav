734328d184ae8dd5edcabe50233266804a7a3ebd
Andrew Hou <andrewhou12@gmail.com>
2025-07-09 12:46:33 -0700

    faster launch time

--- core/systemUIManager.js
+++ core/systemUIManager.js
@@ -2,7 +2,7 @@ const { exec } = require("child_process");
 
 function toggleDockAutohide(enable) {
   return new Promise((resolve, reject) => {
-    const command = `defaults write com.apple.dock autohide -bool ${enable ? "true" : "false"} && killall Dock`;
+    const command = `defaults write com.apple.dock autohide -bool ${enable ? "true" : "false"} && defaults write com.apple.dock autohide-delay -float 0 && defaults write com.apple.dock autohide-time-modifier -float 0.1 && killall Dock`;
 
     exec(command, (error, stdout, stderr) => {
       if (error) {
